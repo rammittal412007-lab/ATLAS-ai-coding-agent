1fdec43f8af8c270591471629d6fbb93e0d20d62
Apratim Das <218270628+uncomparableX@users.noreply.github.com>
2026-05-19 00:33:01 +0530

    Create logging.py

---
+++ backend/app/core/logging.py
@@ -0,0 +1,30 @@
+import logging
+import sys
+
+import structlog
+
+from app.core.config import settings
+
+
+def configure_logging():
+    log_level = logging.DEBUG if settings.DEBUG else logging.INFO
+    logging.basicConfig(format="%(message)s", stream=sys.stdout, level=log_level)
+
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.add_logger_name,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.UnicodeDecoder(),
+            structlog.processors.JSONRenderer()
+            if settings.is_production
+            else structlog.dev.ConsoleRenderer(colors=True),
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(log_level),
+        context_class=dict,
+        logger_factory=structlog.PrintLoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
